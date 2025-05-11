86e96b5b541a1dad0707b7cd9c5b016e8eaa9c80
manojshendge <shendgemanoj878@gmail.com>
2025-05-11 15:17:18 +0530

    Normal changes

--- render-build.sh
+++ render-build.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# 🚨 Create SQLite database file if using SQLite
+# Create SQLite database file if using SQLite
 mkdir -p /var/www/database
 touch /var/www/database/database.sqlite
 
-# ✅ Fix permissions BEFORE Artisan commands
+#  Fix permissions BEFORE Artisan commands
 chmod -R 775 storage bootstrap/cache
 chown -R www-data:www-data storage bootstrap/cache
 
-# 🚀 Laravel setup
+#  Laravel setup
 php artisan config:clear
 php artisan config:cache
 php artisan migrate --force
